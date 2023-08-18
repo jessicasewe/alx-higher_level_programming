d6651edf063755f6c48abeb4b090ce8ec8ef0833
jessicasewe <jessicasewe89@gmail.com>
2023-08-18 04:28:43 -0700

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    biggest = my_list[0]
+    for number in range(len(my_list)):
+        if my_list[number] > biggest:
+            biggest = my_list[number]
+    return(biggest)

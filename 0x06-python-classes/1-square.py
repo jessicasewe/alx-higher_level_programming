bc5f6ddb4c586a29c978cd20b51cf0a3566951bf
jessicasewe <jessicasewe89@gmail.com>
2023-09-11 04:37:13 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size):
+        """initiate a new Sqaure
+
+        Args:
+        size (int): The size of the new sqaure.
+        """
+        self.__size = size

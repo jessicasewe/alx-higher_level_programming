adb81b0d4f015cbb6fb13aace3b6ea151d267995
jessicasewe <jessicasewe89@gmail.com>
2023-11-19 13:18:56 -0800

    adding values to tables

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,25 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3],port=3306)
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_user,
+            passwd=mysql_password,
+            db=mysql_db,
+            port=3306
+    )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT *FROM states ORDER BY id ASC")
+
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
     db.close()

bcf13d3b54e68d92dd587490fa9e7f019984ec86
jessicasewe <jessicasewe89@gmail.com>
2023-11-30 15:23:44 -0800

    tests

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
+"""script for finding peak in list of ints, interview prep
+"""
+
+"""
+    THOUGHT PROCESS
+        it is not sorted, so sorting would take n(log(n))
+            -> not worth sorting
+        looping through and keeping track of max (brute force)
+            -> O(n)
+
+        possibly looping from each end reducing to 1/2 run time
+            -> still O(n)
+"""
 
 
 def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+    """Binary search implementation for finding a peak in a list of unsorted integers.
+    """
+    if not list_of_integers:
         return None
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
+    low, high = 0, len(list_of_integers) - 1
 
-    mid_idx = int(len(list_of_integers) / 2)
+    while low < high:
+        mid = (low + high) // 2
 
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
         else:
-            return list_of_integers[mid_idx - 1]
+            low = mid + 1
 
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+    return list_of_integers[low]
